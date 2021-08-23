c32473f7c4c50651717604952cb1799e9db4a395
arshedgithub <arshedahmedmail@gmail.com>
2021-08-23 06:55:24 +0530

    testing exceptions

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -40,6 +40,9 @@ describe('getProduct', () => {
 
 describe('registerUser', () => {
     it('should throw if username is falsy', () => {
-        expect(() => lib.registerUser(null))
+        const args = [null,undefined,NaN,'',0,false];
+        args.forEach(a => {
+            expect(() => lib.registerUser(a))
+        });
     });
 });
